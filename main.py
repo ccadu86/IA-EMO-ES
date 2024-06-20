3a467dd1b96fc985c8d5d7cc0977e2f6686d6b35
Guilherme Augusto <133021078+Guilherme-Augusto06@users.noreply.github.com>
2024-06-20 17:29:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,5 @@ import os
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'credentials.json'
 
-# Define o caminho para a imagem capturada
-image_path = 'captured_image.png'
-
-# Função que captura a imagem pela webcam
-capture_image(image_path)
-
-# Faz a detecção facial na imagem capturada
-print(detect_faces(image_path))
+# Chama a função para capturar vídeo da webcam
+capture_video()
