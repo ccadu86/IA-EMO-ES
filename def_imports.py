f7b3ea6bbd7af99a99dee6c75b5268b928a7153e
Carlos Ribeiro <36140205+ccadu86@users.noreply.github.com>
2024-06-25 15:03:29 -0300

    Update def_imports.py

--- def_imports.py
+++ def_imports.py
@@ -1,102 +1,104 @@
-import cv2
-from google.cloud import vision
-import numpy as np
-import RPi.GPIO as GPIO
+# Importa as bibliotecas necessárias
+import cv2  # Biblioteca para manipulação de imagens e vídeo
+from google.cloud import vision  # Biblioteca do Google Cloud para detecção de faces
+import numpy as np  # Biblioteca para manipulação de arrays
+import RPi.GPIO as GPIO  # Biblioteca para controle de GPIO no Raspberry Pi
 
-def detectorDeRosto(image_path):
+def detectorDeRosto(arquivoDeImagem):
+    # Cria um cliente para o serviço de detecção de faces do Google Cloud
     client = vision.ImageAnnotatorClient()
 
-    with open(image_path, "rb") as image_file:
+    # Lê a imagem do arquivo
+    with open(arquivoDeImagem, "rb") as image_file:
         content = image_file.read()
 
-    image = vision.Image(content=content)
-    response = client.face_detection(image=image)
+    # Cria um objeto de imagem a partir do conteúdo lido
+    imagem = vision.Image(content=content)
+    
+    # Envia a imagem para o serviço de detecção de faces
+    response = client.face_detection(image=imagem)
     faces = response.face_annotations
 
+    # Verifica se houve algum erro na resposta
     if response.error.message:
         raise Exception(
-            "{}\nFor more info on error messages, check: "
+            "{}\nPara informações ou erros, acesse: "
             "https://cloud.google.com/apis/design/errors".format(response.error.message)
         )
 
+    # Retorna as anotações de faces detectadas
     return faces
 
-def legendasDsenhosRosto(frame, faces):
+def legendasDesenhosRosto(frame, faces):
     for face in faces:
-        vertices = [(vertex.x*3, round(vertex.y*2.25)) for vertex in face.bounding_poly.vertices]
-        pts = np.array(vertices, np.int32)
-        pts = pts.reshape((-1, 1, 2))
-        cv2.polylines(frame, [pts], True, (0, 255, 0), 2)
+        # Redimensiona as coordenadas dos vértices para a escala atual
+        vertices = [(round(vertex.x * 1.5), round(vertex.y * 1.125)) for vertex in face.bounding_poly.vertices]
+        pontos = np.array(vertices, np.int32)
+        pontos = pontos.reshape((-1, 1, 2))
 
-        # Obter a probabilidade mais provável e sua legenda correspondente
-        likelihoods = {
-            'Alegria': face.joy_likelihood,
-            'Tristeza': face.sorrow_likelihood,
-            'Raiva': face.anger_likelihood,
-            'Surpresa': face.surprise_likelihood
+        # Desenha um polígono ao redor da face detectada
+        cv2.polylines(frame, [pontos], True, (0, 255, 0), 2)
+
+        # Obter a probabilidade dos sentimentos e a legenda correspondente
+        probabilidades = {
+            'FELICIDADE': face.joy_likelihood,
+            'TRISTEZA': face.sorrow_likelihood,
+            'RAIVA': face.anger_likelihood,
+            'SURPRESA': face.surprise_likelihood
         }
 
-        # Converter likelihood para porcentagem
-        # likelihood_name = {
-        #     0: "Desconhecido",
-        #     1: "Muito Improvável",
-        #     2: "Improvável",
-        #     3: "Possível",
-        #     4: "Provável",
-        #     5: "Muito Provável"
-        # }
+        # Define um nível de comparação e o texto inicial
+        comparacao = 2
+        texto = "SENTIMENTO: NEUTRO"
+        cores = (0, 0, 0)
+        borda = (0,0,0)
+        # Atualiza o texto com base na probabilidade dos sentimentos
+        for chave, valor in probabilidades.items():
+            if valor > comparacao:
+                texto = f"SENTIMENTO: {chave}"
+                match chave:
+                    case "FELICIDADE":
+                        cores = (0, 255, 255)  # Amarelo (BGR)
+                        borda = (0,0,0)     # PRETO
 
-        likelihood_name = (
-             "Desconhecido",
-             "Muito Improvável",
-             "Improvável",
-             "Possível",
-             "Provável",
-             "Muito Provável"
-        )
+                    case "TRISTEZA":
+                        cores = (255, 0, 0)  # Azul (BGR)
+                        borda = (0,0,0)     # PRETO
 
-        valor =2
-        texto = "SENTIMENTO NEUTRO"
-        if (likelihoods['Alegria']>valor): 
-            valor=likelihoods['Alegria']
-            texto = "SENTIMENTO: FELICIDADE"
-        if (likelihoods['Tristeza']>valor): 
-            valor=likelihoods['Tristeza']
-            texto = "SENTIMENTO: TRISTEZA"
-        if (likelihoods['Raiva']>valor): 
-            valor=likelihoods['Raiva']
-            texto = "SENTIMENTO: RAIVA"
-        if (likelihoods['Surpresa']>valor): 
-            valor=likelihoods['Surpresa']
-            texto = "SENTIMENTO: SURPRESO"
+                    case "RAIVA":
+                        cores = (0, 0, 255)  # Vermelho (BGR)
+                        borda = (0,0,0)     # PRETO
+
+                    case "SURPRESA":
+                        cores = (128, 0, 128)  # Roxo escuro (BGR)
+                        borda = (0,0,0)     # PRETO
+
+                    case _:
+                        cores = (0, 0, 0)
 
 
-        
-        # print("Oir carlão", likelihoods['Alegria'])
+                
 
-        # most_likely_emotion = max(likelihoods, key=likelihoods.get)
-        # most_likely_value = likelihoods[most_likely_emotion]
-        # most_likely_percent = (most_likely_value / 5) * 100
-
-        # # Desenhar as legendas de todas as emoções
-        text_y_offset = 20
-
-        # for emotion, value in likelihoods.items():
-        #     percent = (value / 5) * 100
-        #     color = (0, 255, 0) if emotion == most_likely_emotion else (0, 0, 255)
-        #     text = f"{emotion}: {percent:.2f}% ({likelihood_name[value]})"
-        #     vertice1 = vertices[0][0]
-        #     vertice2 = int(round(vertices[0][1]))
-
-        vertice1 = vertices[0][0]
-        vertice2 = int(round(vertices[0][1]))
+        # Define as posições iniciais e finais para exibir o texto
+        Xinicial = vertices[0][0]
+        Yinicial = vertices[0][1]
+        Xfinal = vertices[3][0]
+        Yfinal = vertices[3][1]
         text = texto
-        color = (255,0, 0)
 
-        cv2.putText(frame, text, (vertice1, vertice2 - text_y_offset), cv2.FONT_HERSHEY_SIMPLEX, 1, color, 3)
-        text_y_offset += 20
+        # Exibe o texto no frame
+        if Yinicial >= 45:
+            # Desenha o contorno preto
+            cv2.putText(frame, text, (Xinicial, Yinicial - 20), cv2.FONT_HERSHEY_SIMPLEX, 1, borda, 6, cv2.LINE_AA)
+            # Desenha o texto colorido
+            cv2.putText(frame, text, (Xinicial, Yinicial - 20), cv2.FONT_HERSHEY_SIMPLEX, 1, cores, 2, cv2.LINE_AA)
+        else:
+            # Desenha o contorno preto
+            cv2.putText(frame, text, (Xfinal, Yfinal + 40), cv2.FONT_HERSHEY_SIMPLEX, 1, borda, 6, cv2.LINE_AA)
+            # Desenha o texto colorido
+            cv2.putText(frame, text, (Xfinal, Yfinal + 40), cv2.FONT_HERSHEY_SIMPLEX, 1, cores, 2, cv2.LINE_AA)
 
-def capture_video():
+def capturaDeVideo():
     # Inicializa a captura de vídeo da webcam
     cap = cv2.VideoCapture(0)
     GPIO.setmode(GPIO.BCM)
@@ -112,20 +114,20 @@ def capture_video():
     cv2.setWindowProperty('Webcam', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
     # Define a resolução da tela
-    screen_width = 1920
-    screen_height = 1080
+    larguraDaTela = 1920
+    alturaDaTela = 1080
 
     while True:
-        button_state = GPIO.input(BUTTON_PIN)
+        estadoDoBotao = GPIO.input(BUTTON_PIN)
 
         ret, frame = cap.read()
         if not ret:
             break
 
         # Redimensiona o frame para preencher a tela
-        frame_resized = cv2.resize(frame, (screen_width, screen_height), interpolation=cv2.INTER_LINEAR)
+        frame_resized = cv2.resize(frame, (larguraDaTela, alturaDaTela), interpolation=cv2.INTER_LINEAR)
 
-        if button_state == 0:
+        if estadoDoBotao == 0:
             # Exibe o frame redimensionado na janela
             cv2.imshow('Webcam', frame_resized)
 
@@ -134,28 +136,30 @@ def capture_video():
             break
             
         # Se o botão for pressionado, capture e exiba a imagem
-        if button_state == 1:
+        if estadoDoBotao == 1:
             # Define o caminho para salvar a imagem capturada
-            image_path = 'captured_image.jpg'
-            cv2.imwrite(image_path, frame)
+            arquivoDeImagem = 'captured_image.jpg'
+            cv2.imwrite(arquivoDeImagem, frame)
 
-            faces = detectorDeRosto(image_path)
-            print(faces)
+            # Detecta as faces na imagem capturada
+            faces = detectorDeRosto(arquivoDeImagem)
+        
             # Exibe a imagem capturada
-            captured_frame = cv2.imread(image_path)
-            captured_frame_resized = cv2.resize(captured_frame, (screen_width, screen_height), interpolation=cv2.INTER_LINEAR)
-            
-            image = captured_frame_resized.astype(np.uint8)
+            capturarFrame = cv2.imread(arquivoDeImagem)
+            frame_height, frame_width, _ = capturarFrame.shape
 
+            # Redimensiona a imagem capturada
+            imagem = cv2.resize(capturarFrame, (larguraDaTela, alturaDaTela), interpolation=cv2.INTER_LINEAR)
+        
             # Desenha as legendas e os desenhos no frame capturado
-            legendasDsenhosRosto(image, faces)
+            legendasDesenhosRosto(imagem, faces)
             
-            cv2.imshow('Captura', image)
+            cv2.imshow('Captura', imagem)
             cv2.waitKey(1)  # Atualiza a tela
             
-            while button_state == 1:
-                button_state = GPIO.input(BUTTON_PIN)
-                cv2.imshow('Captura', image)
+            while estadoDoBotao == 1:
+                estadoDoBotao = GPIO.input(BUTTON_PIN)
+                cv2.imshow('Captura', imagem)
                 cv2.waitKey(1)  # Atualiza a tela
         
             cv2.destroyWindow('Captura')  # Fecha a janela da captura
@@ -164,5 +168,3 @@ def capture_video():
     cap.release()
     cv2.destroyAllWindows()
     GPIO.cleanup()
-
-
